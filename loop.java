69fdd555abb30d8fa7210843b54921d094667b9e
DharshiniT <tkp.dharshini@gmail.com>
2017-06-12 15:35:28 +0530

    Create loop.java

---
+++ loop.java
@@ -0,0 +1,16 @@
+import java.io.*;
+import java.util.*;
+class loop
+{
+	public static void main(String args[])
+	{
+		int sum=0,i;
+
+		for(i=0;i<100;i++)
+		{
+			
+			sum=sum+i;
+			System.out.print(sum);
+		}
+		}
+	}
